2cddf58493b1f0895ba9f1311c785cba5348e64e
dzemildupljak <dzemildupljak4795@gmail.com>
2022-03-11 01:53:36 +0100

    add entity donors_event and rep impl

---
+++ server/usecase/donation_event/donation_event_repository.go
@@ -0,0 +1,15 @@
+package donor_usecase
+
+import (
+	"context"
+
+	"github.com/dzemildupljak/risc_monolith/server/domain"
+)
+
+type DonationEventRepository interface {
+	DonationEventsList(
+		ctx context.Context,
+		arg domain.DonationEventsListParams) ([]domain.DonationEvent, error)
+	DonationEventById(
+		ctx context.Context, eventID int64) (domain.DonationEvent, error)
+}
